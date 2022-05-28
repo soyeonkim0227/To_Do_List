2530ef921fa1b8e2203dfc97c9c9909146ebddb8
soyeonkim0227 <soyeonkim0227@naver.com>
2022-05-28 14:14:18 +0900

    Update todo controller
    create todo API

--- src/main/java/com/soyeonkim/todolist/controller/TodoController.java
+++ src/main/java/com/soyeonkim/todolist/controller/TodoController.java
@@ -1,6 +1,21 @@
 package com.soyeonkim.todolist.controller;
 
+import com.soyeonkim.todolist.controller.dto.CreateTodoRequest;
+import com.soyeonkim.todolist.service.TodoService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/todo")
 public class TodoController {
 
+    private final TodoService todoService;
 
+    @PostMapping
+    @ResponseStatus(value = HttpStatus.CREATED)
+    public void creatUser(@RequestBody CreateTodoRequest dto) {
+        todoService.createTodo(dto);
+    }
 }
