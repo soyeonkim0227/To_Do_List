d4b11ec4170686348d966d0996c575be28ebc53f
soyeonkim0227 <soyeonkim0227@naver.com>
2022-06-09 23:42:32 +0900

    Create signup entity

---
+++ src/main/java/com/soyeonkim/todolist/entity/User.java
@@ -0,0 +1,32 @@
+package com.soyeonkim.todolist.entity;
+
+import lombok.*;
+
+import javax.persistence.*;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "table_user")
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Setter
+    private String name;
+
+    @Setter
+    private Integer number;
+
+    @Setter
+    private Integer badScore;
+
+    @Setter
+    private String accountId;
+
+    @Setter
+    private String accountPassword;
+}
