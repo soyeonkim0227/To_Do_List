96d607d714b72e7bf404e61cc34085bfd58749cc
soyeonkim0227 <soyeonkim0227@naver.com>
2022-06-10 15:28:03 +0900

    Update login entity

--- src/main/java/com/soyeonkim/todolist/entity/UserRepository.java
+++ src/main/java/com/soyeonkim/todolist/entity/UserRepository.java
@@ -3,6 +3,10 @@ package com.soyeonkim.todolist.entity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer> {
+
+    Optional<User> findByAccountId(String accountId);
 }
